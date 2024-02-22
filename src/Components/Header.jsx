c60772460b9391ad676a1605e3fb2c96f84d7ac4
Damian <olebueziedamian@gmail.com>
2024-02-22 13:17:25 +0100

    build 2.0

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="flex justify-between items-center py-2 mb-8">
-      <div className="text-3xl px-4">mySITE</div>
+      <div className="text-3xl px-4">my SITE</div>
       <nav className="flex gap-2 mr-4">
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
